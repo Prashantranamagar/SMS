a170c38825c5e44fe65041ed902950f8c338f426
Prashantranamagar <ranamagar.prashant@gmail.com>
2024-06-26 04:59:34 +0545

    CRUD for Enrollment, Student, Instructor, Enrollment

---
+++ app_sms/views.py
@@ -0,0 +1,244 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+    DetailView
+)
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth import login as auth_login
+from django.views.generic import FormView
+from django.db.models import Q
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .models import Student, Course, Enrollment, Instructor
+from .forms import (
+    StudentForm,
+    CourseForm,
+    EnrollmentForm,
+    InstructorForm,
+    CustomUserCreationForm
+)
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+class SignupView(FormView):
+    template_name = 'signup.html'
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        user = form.save()
+        auth_login(self.request, user)
+        return super().form_valid(form)
+
+
+class CustomLogoutView(LogoutView):
+    template_name = 'logged_out.html'
+
+
+class CustomLoginView(LoginView):
+    template_name = 'login.html'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return self.get_redirect_url() or '/'
+
+
+class StudentListView(ListView):
+    model = Student
+    template_name = 'students/student_list.html'
+    context_object_name = 'students'
+    paginate_by = 5
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        query = self.request.GET.get('q')
+        if query:
+            queryset = queryset.filter(
+                Q(name__icontains=query) |
+                Q(email__icontains=query) 
+            )
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['query'] = self.request.GET.get('q', '')
+        return context
+
+
+class StudentCreateView(LoginRequiredMixin, CreateView):
+    model = Student
+    form_class = StudentForm
+    template_name = 'students/student_form.html'
+    success_url = reverse_lazy('student-list')
+
+
+class StudentUpdateView(LoginRequiredMixin, UpdateView):
+    model = Student
+    form_class = StudentForm
+    template_name = 'students/student_form.html'
+    success_url = reverse_lazy('student-list')
+
+
+class StudentDeleteView(LoginRequiredMixin, DeleteView):
+    model = Student
+    template_name = 'students/student_confirm_delete.html'
+    success_url = reverse_lazy('student-list')
+
+
+class StudentDetailView(LoginRequiredMixin, DetailView):
+    model = Student
+    template_name = 'students/student_detail.html'
+
+
+#Course View
+class CourseListView(ListView):
+    model = Course
+    template_name = 'courses/course_list.html'
+    context_object_name = 'courses'
+    paginate_by = 5  
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        query = self.request.GET.get('q')
+        if query:
+            queryset = queryset.filter(
+                Q(course_name__icontains=query) |
+                Q(course_code__icontains=query) 
+            )
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['query'] = self.request.GET.get('q', '')
+        return context
+
+
+class CourseDetailView(LoginRequiredMixin, DetailView):
+    model = Course
+    template_name = 'courses/course_detail.html'
+
+
+class CourseCreateView(LoginRequiredMixin, CreateView):
+    model = Course
+    form_class = CourseForm
+    template_name = 'courses/course_form.html'
+    success_url = reverse_lazy('course-list')
+
+
+class CourseUpdateView(LoginRequiredMixin, UpdateView):
+    model = Course
+    form_class = CourseForm
+    template_name = 'courses/course_form.html'
+    success_url = reverse_lazy('course-list')
+
+
+class CourseDeleteView(LoginRequiredMixin, DeleteView):
+    model = Course
+    template_name = 'courses/course_confirm_delete.html'
+    success_url = reverse_lazy('course-list')
+
+
+#Enrolments View
+class EnrollmentListView(ListView):
+    model = Enrollment
+    template_name = 'enrollments/enrollment_list.html'
+    context_object_name = 'enrollments'
+    paginate_by = 2  
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        query = self.request.GET.get('q')
+        if query:
+            queryset = queryset.filter(
+                Q(student__icontains=query) |
+                Q(course__icontains=query) 
+            )
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['query'] = self.request.GET.get('q', '')
+        return context
+
+
+class EnrollmentDetailView(LoginRequiredMixin, DetailView):
+    model = Enrollment
+    template_name = 'enrollments/enrollment_detail.html'
+    context_object_name = 'enrollment'
+
+
+class EnrollmentCreateView(LoginRequiredMixin, CreateView):
+    model = Enrollment
+    form_class = EnrollmentForm
+    template_name = 'enrollments/enrollment_form.html'
+    success_url = reverse_lazy('enrollment-list')
+
+
+class EnrollmentUpdateView(LoginRequiredMixin, UpdateView):
+    model = Enrollment
+    form_class = EnrollmentForm
+    template_name = 'enrollments/enrollment_form.html'
+    success_url = reverse_lazy('enrollment-list')
+
+
+class EnrollmentDeleteView(LoginRequiredMixin, DeleteView):
+    model = Enrollment
+    template_name = 'enrollments/enrollment_confirm_delete.html'
+    success_url = reverse_lazy('enrollment-list')
+
+
+#Instructor View
+class InstructorListView(ListView):
+    model = Instructor
+    template_name = 'instructors/instructor_list.html'
+    context_object_name = 'instructors'
+    paginate_by = 3  
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        query = self.request.GET.get('q')
+        if query:
+            queryset = queryset.filter(
+                Q(first_name__icontains=query) |
+                Q(last_name__icontains=query) 
+            )
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['query'] = self.request.GET.get('q', '')
+        return context
+
+
+class InstructorDetailView(LoginRequiredMixin, DetailView):
+    model = Instructor
+    template_name = 'instructors/instructor_detail.html'
+    context_object_name = 'instructor'
+
+
+class InstructorCreateView(LoginRequiredMixin, CreateView):
+    model = Instructor
+    form_class = InstructorForm
+    template_name = 'instructors/instructor_form.html'
+    success_url = reverse_lazy('instructor-list')
+
+
+class InstructorUpdateView(LoginRequiredMixin, UpdateView):
+    model = Instructor
+    form_class = InstructorForm
+    template_name = 'instructors/instructor_form.html'
+    success_url = reverse_lazy('instructor-list')
+
+
+class InstructorDeleteView(LoginRequiredMixin, DeleteView):
+    model = Instructor
+    template_name = 'instructors/instructor_confirm_delete.html'
+    success_url = reverse_lazy('instructor-list')
